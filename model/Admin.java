9f5a26bef5e6bd3faaac4f75d525fef17bcb3a0e
Md. Ashikur Rahman Siyam <149653672+siyam119388@users.noreply.github.com>
2026-08-17 12:08:52 +0600

    Add Admin class extending User with role and path

---
+++ model/Admin.java
@@ -0,0 +1,12 @@
+package com.unimate.model;
+
+import com.unimate.util.Constants;
+
+public class Admin extends User {
+
+    @Override
+    public String getRole() { return Constants.ROLE_ADMIN; }
+
+    @Override
+    public String getDashboardPath() { return "admin/dashboard.html"; }
+}
