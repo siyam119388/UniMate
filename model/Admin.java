04d5805ffc5e613c15f12bc16a05bfb41a147b9a
Md. Ashikur Rahman Siyam <149653672+siyam119388@users.noreply.github.com>
2026-08-18 13:21:16 -0700

    Fix formatting in Admin class getRole method

--- model/Admin.java
+++ model/Admin.java
@@ -5,8 +5,8 @@ import com.unimate.util.Constants;
 public class Admin extends User {
 
     @Override
-    public String getRole() { return Constants.ROLE_ADMIN; }
+    public String getRole() { return Constants.ROLE_ADMIN; }   ///
 
     @Override
     public String getDashboardPath() { return "admin/dashboard.html"; }
-}
+} 
